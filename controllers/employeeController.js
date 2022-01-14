8955e358e492787b54e8ba2de14be6db61ac9136
cyqg <cyqg@fresnou.org>
2022-01-14 08:55:34 -0800

    second commit

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -11,6 +11,55 @@ module.exports.renderAddEmployeeForm = function(req, res) {
     res.render('createUserForm',
         {
             jobtitles,
-            stateslist:state
+            stateslist:states
         });
+}
+
+module.exports.addEmployee = async function(req, res) {
+    await Employee.create(
+            {
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            jobtitle:req.body.jobtitle,
+            streetline1:req.body.streetline1,
+            streetline2:req.body.streetline2,
+            city:req.body.city,
+            state:req.body.state,
+            zip:req.body.zip,
+            phonenumber:req.body.phonenumber,
+            yearhired:req.body.yearhired
+        }
+    );
+    res.redirect('/');
+}
+
+module.exports.renderUpdateForm = async function(req, res) {
+    const employee = await Employee.findByPk(req.params.id);
+    res.render('edit', {
+        employee,
+        jobtitles,
+        stateslist:states
+    });
+}
+
+module.exports.updateEmployee = async function(req, res) {
+    await Employee.update(
+        {
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            jobtitle:req.body.jobtitle,
+            streetline1:req.body.streetline1,
+            streetline2:req.body.streetline2,
+            city:req.body.city,
+            state:req.body.state,
+            zip:req.body.zip,
+            phonenumber:req.body.phonenumber,
+            yearhired:req.body.yearhired
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        });
+    res.redirect('/');
 }
